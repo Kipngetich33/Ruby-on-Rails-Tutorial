180317c917804ee70c244d7fe7656e80bd33108d
Kipngetich33 <khalifngeno@gmail.com>
2022-08-17 12:10:25 +0300

    add functionality to cascade on delete articles to delete all the comments that belong to that article:

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,5 +1,8 @@
 class Article < ApplicationRecord
     include Visible #include the concern
     # add relationship with the Article's records
-    has_many :comments
+    has_many :comments,dependent: :destroy
+    # add validators below
+    validates :title, presence: true
+    validates :body, presence: true, length: { minimum: 10 }
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,4 +1,7 @@
 class Comment < ApplicationRecord
   include Visible #include concern
   belongs_to :article
+  # add validators below
+  validates :title, presence: true
+  validates :body, presence: true, length: { minimum: 10 }
 end

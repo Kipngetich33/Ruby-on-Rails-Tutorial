c3b7b0139bff1cee69e5b0dfe19ceae75f03f5ee
Kipngetich33 <khalifngeno@gmail.com>
2022-08-17 10:47:16 +0300

    add concerns to make the code DRY

--- app/models/article.rb
+++ app/models/article.rb
@@ -1,7 +1,5 @@
 class Article < ApplicationRecord
+    include Visible #include the concern
     # add relationship with the Article's records
     has_many :comments
-    # add field validation below
-    validates :title, presence: true
-    validates :body, presence: true, length: { minimum: 10}
 end
--- app/models/comment.rb
+++ app/models/comment.rb
@@ -1,3 +1,4 @@
 class Comment < ApplicationRecord
+  include Visible #include concern
   belongs_to :article
 end
